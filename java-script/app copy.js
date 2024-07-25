64e43c89814e67e698d2b72698225f98cdb03944
L1ght777 <ungerer.cp@gmail.com>
2024-07-25 20:25:56 +0200

    Remove File

--- java-script/app copy.js
+++
@@ -1,7 +0,0 @@
-// GLOBAL VARIABLES
-
-// __dirname    - PATH TO CURRENT DIRECTORY
-// __filename   - FILE NAME
-// require      - function to use modules (CommonJS)
-// module       - info about current module (file)
-// process      - info about end where the program is begin executed.
