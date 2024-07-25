1a98a0faf7ecf82b6fba41cf1464ae8720a2a74e
L1ght777 <ungerer.cp@gmail.com>
2024-07-25 20:17:42 +0200

    My commit

---
+++ java-script/1-intro.js
@@ -0,0 +1,10 @@
+const amount = 12;
+
+if(amount < 10){
+    console.log('amount is less than 10');
+}
+else{
+    console.log('number larger than 10');
+}
+
+console.log('This is my first nodeJS app')
