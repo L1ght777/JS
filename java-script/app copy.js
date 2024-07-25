6c7a7d6ab0c02909e78ae1b71669d294d426130e
L1ght777 <ungerer.cp@gmail.com>
2024-07-25 20:21:12 +0200

    Test

---
+++ java-script/app copy.js
@@ -0,0 +1,7 @@
+// GLOBAL VARIABLES
+
+// __dirname    - PATH TO CURRENT DIRECTORY
+// __filename   - FILE NAME
+// require      - function to use modules (CommonJS)
+// module       - info about current module (file)
+// process      - info about end where the program is begin executed.
